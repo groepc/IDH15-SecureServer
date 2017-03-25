e4ae9bc9986a98cd203486f4fd4c85b6256abe12
Nymus5 <mvdoorneveld@gmail.com>
2017-03-25 16:15:54 +0100

    Update

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,18 @@ namespace Server
 {
     class Program
     {
-    }
+        
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Server server = new Server(IPAddress, port);
+                server.start();
+            }
+            catch (Exception e)
+            {
+                System.err.println(e.getMessage());
+            }
+        }
 }
