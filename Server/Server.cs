6e22db633c3fb3730a011388cb7f2eb234816ee9
fdmdubois <fdmdubois@gmail.com>
2017-03-25 15:10:22 +0100

    Server update

---
+++ Server/Server.cs
@@ -0,0 +1,53 @@
+﻿using Server.request;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+
+
+    public class Server
+    {
+
+        static void Main(string[] args)
+        {
+            TcpListener server = null;
+            try
+            {
+                // Set the TcpListener on port 13000.
+                Int32 port = 13000;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localAddr, port);
+                RequestHandler handler = new RequestHandler(server);
+                // Start listening for client requests.
+                server.Start();
+
+                // Enter the listening loop.
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+
+
+
+                    Console.WriteLine("Connected!");
+
+                }
+
+            }
+            catch (SocketException e)
+            {
+            }
+            catch (IOException ex)
+            {
+            }
+            finally
+            {
+            }
+
+        }
+    }
+}
---
+++ Server/request/RequestHandler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.request
+{
+    public class RequestHandler 
+    {
+        private TcpListener socket;
+        private StreamWriter socket_in;
+        private StreamReader socket_out;
+
+        private Stream stream;
+
+        private String webroot = "C:/webroot";
+
+        public RequestHandler(TcpListener socket)
+        {
+            this.socket = socket;
+        }
+
+        public void run()
+        {
+            try
+            {
+                socket_in = socket.; //socket.getInputStream();
+                socket_out = socket.getOutputStream();
+                request = new Request(socket_in);
+
+                MyFile myfile = new MyFile(webroot + request.getPath());
+
+
+                writeFile(myfile);
+                socket.close();
+            }
+            catch (BadRequestException e)
+            {
+                writeError(400);
+            }
+            catch (FileNotFoundException e)
+            {
+                writeError(404);
+            }
+            catch (IOException e)
+            {
+                writeError(500);
+            }
+        }
+    }
+}
