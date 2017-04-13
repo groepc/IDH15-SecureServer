20d0b5e74eae61667683a8f71c84904fdc7b6684
Perry Faro <perryfaro@live.nl>
2017-04-13 09:14:51 +0200

    Default page toegeveogd

--- Server/utils/Myfile.cs
+++ Server/utils/Myfile.cs
@@ -1,159 +1,172 @@
 ﻿using System;
 using System.IO;
+using Server.utils;
 
 namespace Server.Entities
 {
-    public class MyFile
-    {
-        string fullPath;
-        string[] pathParts;
-        string name;
-        string ext;
-        FileStream inStream;
-        Boolean indexPage = false;
+	public class MyFile
+	{
+		string fullPath;
+		string[] pathParts;
+		string name;
+		string ext;
+		FileStream inStream;
+		Boolean indexPage = true;
 
-        /**
-         * Get the input stream
-         * @return
-         * @throws FileNotFoundException 
-         */
-        private FileStream GetFileInputStream()
-        {
-            if (inStream == null)
-            {
-                inStream = new FileStream(fullPath, FileMode.Open);
-            }
-            return inStream;
-        }
+		/**
+		 * Get the input stream
+		 * @return
+		 * @throws FileNotFoundException 
+		 */
+		private FileStream GetFileInputStream()
+		{
+			if (inStream == null)
+			{
+				inStream = new FileStream(fullPath, FileMode.Open);
+			}
+			return inStream;
+		}
 
-        /**
-         * Constructor
-         * parse filename
-         * @param path 
-         */
-        public MyFile(string path)
-        {
-            fullPath = path;
-            if (fullPath.EndsWith("/"))
-            {
-                fullPath += "index.html";
-                this.checkIfIndexPageExists(fullPath);
-            }
+		/**
+		 * Constructor
+		 * parse filename
+		 * @param path 
+		 */
+		public MyFile(string path)
+		{
+			fullPath = path;
+			if (!Path.HasExtension(fullPath))
+			{
+				string orginalPath = fullPath;
+				string[] defaultPages = AppConfigProcessor.Get().DefaultPages.Split(new string[] { ";" }, StringSplitOptions.None);
+				Boolean result = false;
+				foreach (string i in defaultPages)
+				{
+					fullPath = orginalPath + "/" + i;
+					result = this.checkIfIndexPageExists(fullPath);
+					if (result == true)
+					{
+						break;
+					}
+				}
+			} else {
+				indexPage = false;
+			}
 
-            pathParts = fullPath.Split('/');
-            if (pathParts.Length > 0)
-            {
-                name = pathParts[pathParts.Length - 1];
-            }
-            else
-            {
-                name = "";
-            }
-        }
+				pathParts = fullPath.Split('/');
+			if (pathParts.Length > 0)
+			{
+				name = pathParts[pathParts.Length - 1];
+			}
+			else
+			{
+				name = "";
+			}
+		}
 
-        protected void checkIfIndexPageExists(String fullPath)
-        {
-            Console.WriteLine(fullPath);
-            if (!File.Exists(fullPath))
-            {
-                indexPage = true;
+		protected Boolean checkIfIndexPageExists(String fullPath)
+		{
+			if (!File.Exists(fullPath))
+			{
+				return false;
+			}
+			indexPage = false;
+			return true;
+		}
 
-            }
-        }
+		public Boolean indexPageExists()
+		{
+			return indexPage;
+		}
 
-        public Boolean indexPageExists()
-        {
-            return indexPage;
-        }
+		public override string ToString()
+		{
+			return fullPath;
+		}
 
-        public override string ToString()
-        {
-            return fullPath;
-        }
+		/**
+		 * 
+		 * @return file name including extension
+		 */
+		public string GetName()
+		{
+			return name;
+		}
 
-        /**
-         * 
-         * @return file name including extension
-         */
-        public string GetName()
-        {
-            return name;
-        }
+		/**
+		 * 
+		 * @return file extension, including dot
+		 */
+		public string GetExtension()
+		{
+			if (ext == null)
+			{
+				int pos = name.LastIndexOf('.');
+				if (pos < 0)
+				{
+					ext = "";
+				}
+				else
+				{
+					ext = name.Substring(pos);
+				}
+			}
+			return ext;
+		}
 
-        /**
-         * 
-         * @return file extension, including dot
-         */
-        public string GetExtension()
-        {
-            if (ext == null)
-            {
-                int pos = name.LastIndexOf('.');
-                if (pos < 0)
-                {
-                    ext = "";
-                }
-                else
-                {
-                    ext = name.Substring(pos);
-                }
-            }
-            return ext;
-        }
+		/**
+		 * Read bytes from file
+		 * @param buffer: buffer to read to
+		 * @param maxlen: maximum number of bytes to read
+		 * @return number of bytes actually read, -1 means end of file
+		 * @throws FileNotFoundException
+		 * @throws IOException 
+		 */
+		public long Read(byte[] buffer, int maxlen)
+		{
+			GetFileInputStream();
+			return inStream.Read(buffer, 0, maxlen);
+		}
 
-        /**
-         * Read bytes from file
-         * @param buffer: buffer to read to
-         * @param maxlen: maximum number of bytes to read
-         * @return number of bytes actually read, -1 means end of file
-         * @throws FileNotFoundException
-         * @throws IOException 
-         */
-        public long Read(byte[] buffer, int maxlen)
-        {
-            GetFileInputStream();
-            return inStream.Read(buffer, 0, maxlen);
-        }
+		/**
+		 * Get the content type, determined by file extension (may be unreliable)
+		 * @return content type as used in http headers
+		 */
+		public string GetContentType()
+		{
+			string lext = GetExtension().ToLower();
+			switch (lext)
+			{
+				case ".pdf":
+					return "application/pdf";
+				case ".htm":
+				case ".html":
+					return "text/html; charset=UTF-8";
+				case ".gif":
+					return "image/gif";
+				case ".jpg":
+				case ".jpeg":
+					return "image/jpeg";
+				default:
+					return "application/octet-stream\r\nContent-Disposition: attachment; filename=\"" + GetName() + "\"";
+			}
+		}
 
-        /**
-         * Get the content type, determined by file extension (may be unreliable)
-         * @return content type as used in http headers
-         */
-        public string GetContentType()
-        {
-            string lext = GetExtension().ToLower();
-            switch (lext)
-            {
-                case ".pdf":
-                    return "application/pdf";
-                case ".htm":
-                case ".html":
-                    return "text/html; charset=UTF-8";
-                case ".gif":
-                    return "image/gif";
-                case ".jpg":
-                case ".jpeg":
-                    return "image/jpeg";
-                default:
-                    return "application/octet-stream\r\nContent-Disposition: attachment; filename=\"" + GetName() + "\"";
-            }
-        }
+		public long GetLength()
+		{
+			GetFileInputStream();
+			// FileAccess.
 
-        public long GetLength()
-        {
-            GetFileInputStream();
-            // FileAccess.
+			return inStream.Length;
+			//  inStream.GetChannel().size();
+		}
 
-            return inStream.Length;
-            //  inStream.GetChannel().size();
-        }
-
-        public void Close()
-        {
-            if (inStream != null)
-            {
-                inStream.Close();
-            }
-        }
-    }
+		public void Close()
+		{
+			if (inStream != null)
+			{
+				inStream.Close();
+			}
+		}
+	}
 }
